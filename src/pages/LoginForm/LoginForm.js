545038eb3e2ce0821cba50f51835ecccc4527fd8
Vítor Moreira Rodrigues <vitorrdrgs04@gmail.com>
2025-05-19 11:00:37 -0300

    Added register and login routes. Connected Login and Register with back-end

--- src/pages/LoginForm/LoginForm.js
+++ src/pages/LoginForm/LoginForm.js
@@ -2,11 +2,40 @@ import { useNavigate } from "react-router-dom";
 import EmailIcon from "../../assets/icons/EmailIcon/EmailIcon.js";
 import KeyIcon from "../../assets/icons/KeyIcon/KeyIcon.js";
 import styles from "./LoginForm.module.scss";
+import { useState } from "react";
 
 function LoginForm() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleBack = () => navigate("/");
+
+  function entrar() {
+    const apiUrl = process.env.REACT_APP_API_URL;
+
+    fetch(`${apiUrl}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then(async (res) => {
+        const data = await res.json();
+
+        if (!res.ok) {
+          alert(data.erro || "Erro desconhecido no login.");
+          return;
+        }
+
+        localStorage.setItem("token", data.token);
+        navigate("/user/graphs");
+      })
+      .catch(() => {
+        alert("Erro de conexão com o servidor.");
+      });
+  }
 
   return (
     <div className={styles.authContainer}>
@@ -19,7 +48,13 @@ function LoginForm() {
           <span className={styles.icon}>
             <EmailIcon />
           </span>
-          <input type="email" placeholder="Email" autoComplete="email" />
+          <input
+            type="email"
+            placeholder="Email"
+            autoComplete="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div className={styles.inputGroup}>
@@ -30,11 +65,15 @@ function LoginForm() {
             type="password"
             placeholder="Senha"
             autoComplete="current-password"
+            value={password}
+            onChange={(s) => setPassword(s.target.value)}
           />
         </div>
 
         <div className={styles.buttonGroup}>
-          <button className={styles.primaryBtn}>Entrar</button>
+          <button className={styles.primaryBtn} onClick={entrar}>
+            Entrar
+          </button>
           <button className={styles.linkBtn}>Recuperar Senha</button>
           <button className={styles.linkBtn} onClick={handleBack}>
             Voltar
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,19 @@
-import { Routes, Route } from 'react-router';
-import DrawScreen from './pages/DrawScreen/DrawScreen.js'
-import UserGraphs from './pages/UserGraphs/UserGraphs.js';
+import { Routes, Route } from "react-router";
+import DrawScreen from "./pages/DrawScreen/DrawScreen.js";
+import UserGraphs from "./pages/UserGraphs/UserGraphs.js";
+import LoginForm from "./pages/LoginForm/LoginForm.js";
+import RegisterForm from "./pages/RegisterForm/RegisterForm.js";
 
-function AppRoutes(){
-    return (
-        <Routes>
-            <Route path="/draw" element={<DrawScreen/>}/>
-            <Route path="/user/graphs" element={<UserGraphs/>}/>
-        </Routes>
-    );
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/draw" element={<DrawScreen />} />
+      <Route path="/user/graphs" element={<UserGraphs />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+    </Routes>
+  );
 }
 
 export default AppRoutes;
+
